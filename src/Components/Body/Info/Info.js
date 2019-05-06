71dc94523672c4737a28f3d41906072c74248a9d
Stephane <stephane.achari@u-psud.fr>
2019-05-06 18:41:57 +0200

    User info finish

--- src/Components/Body/Info/Info.js
+++ src/Components/Body/Info/Info.js
@@ -44,92 +44,95 @@ export default class InFO extends Component {
             <button onClick={this.enableInput}>Modifier</button>
           </div>
 
-          <div className="displayForm">
-            <label>
-              Nom :
-              <br />
-              <input
-                className={inputStyle[0] + ' marginInput '}
-                type="text"
-                name="nom"
-                onChange={this.handleInput}
-                value={this.state.nom}
-                disabled={this.state.enableInputBool}
-              />
-            </label>
-            <label>
-              Prénom :
-              <br />
-              <input
-                className={inputStyle[0] + ' marginInput '}
-                type="text"
-                name="prenom"
-                onChange={this.handleInput}
-                value={this.state.prenom}
-                disabled={this.state.enableInputBool}
-              />
-            </label>
-            <label>
-              Sexe :
-              <br />
-              <input
-                className={inputStyle[0] + ' marginInput '}
-                type="text"
-                name="sexe"
-                onChange={this.handleInput}
-                value={this.state.sexe}
-                disabled={this.state.enableInputBool}
-              />
-            </label>
-            <label>
-              Téléphone :
-              <br />
-              <input
-                className={inputStyle[0] + ' marginInput '}
-                id="inputTest"
-                type="text"
-                name="telephone"
-                onChange={this.handleInput}
-                value={this.state.telephone}
-                disabled={this.state.enableInputBool}
-              />
-            </label>
-            <label>
-              Adresse :
-              <br />
-              <textarea
-                className={inputStyle[0] + ' marginInput '}
-                type="text"
-                name="adresse"
-                onChange={this.handleInput}
-                value={this.state.adresse}
-                disabled={this.state.enableInputBool}
-              />
-            </label>
-            <label>
-              Adresse de facturation :
-              <br />
-              <textarea
-                className={inputStyle[0] + ' marginInput '}
-                type="text"
-                name="adresseFactu"
-                onChange={this.handleInput}
-                value={this.state.adresseFactu}
-                disabled={this.state.enableInputBool}
-              />
-            </label>
-            <label>
-              Adresse de livraison :
-              <br />
-              <textarea
-                className={inputStyle[0] + ' marginInput '}
-                type="text"
-                name="adresseLivraison"
-                onChange={this.handleInput}
-                value={this.state.adresseLivraison}
-                disabled={this.state.enableInputBool}
-              />
-            </label>
+          <div>
+            <div className="displayForm">
+              <label>
+                Nom :
+                <br />
+                <input
+                  className={inputStyle[0] + ' marginInput '}
+                  type="text"
+                  name="nom"
+                  onChange={this.handleInput}
+                  value={this.state.nom}
+                  disabled={this.state.enableInputBool}
+                />
+              </label>
+              <label>
+                Prénom :
+                <br />
+                <input
+                  className={inputStyle[0] + ' marginInput '}
+                  type="text"
+                  name="prenom"
+                  onChange={this.handleInput}
+                  value={this.state.prenom}
+                  disabled={this.state.enableInputBool}
+                />
+              </label>
+              <label>
+                Sexe :
+                <br />
+                <input
+                  className={inputStyle[0] + ' marginInput '}
+                  type="text"
+                  name="sexe"
+                  onChange={this.handleInput}
+                  value={this.state.sexe}
+                  disabled={this.state.enableInputBool}
+                />
+              </label>
+              <label>
+                Téléphone :
+                <br />
+                <input
+                  className={inputStyle[0] + ' marginInput '}
+                  id="inputTest"
+                  type="text"
+                  name="telephone"
+                  onChange={this.handleInput}
+                  value={this.state.telephone}
+                  disabled={this.state.enableInputBool}
+                />
+              </label>
+              <label>
+                Adresse :
+                <br />
+                <textarea
+                  className={inputStyle[0] + ' marginInput '}
+                  type="text"
+                  name="adresse"
+                  onChange={this.handleInput}
+                  value={this.state.adresse}
+                  disabled={this.state.enableInputBool}
+                />
+              </label>
+              <label>
+                Adresse de facturation :
+                <br />
+                <textarea
+                  className={inputStyle[0] + ' marginInput '}
+                  type="text"
+                  name="adresseFactu"
+                  onChange={this.handleInput}
+                  value={this.state.adresseFactu}
+                  disabled={this.state.enableInputBool}
+                />
+              </label>
+              <label>
+                Adresse de livraison :
+                <br />
+                <textarea
+                  className={inputStyle[0] + ' marginInput '}
+                  type="text"
+                  name="adresseLivraison"
+                  onChange={this.handleInput}
+                  value={this.state.adresseLivraison}
+                  disabled={this.state.enableInputBool}
+                />
+              </label>
+            </div>
+
             <div className={inputStyle[1] + ' btnCenter '}>
               <button>Sauvegarder</button>
               <button className="btnQuit" onClick={this.enableInput}>
